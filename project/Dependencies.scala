b1116d6500cac2cb2017626e65266e7373223050
Jorge Vicente Cantero <jorgevc@fastmail.es>
2020-11-28 20:45:26 +0100

    Upgrade to latest JNA

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val Scala212Version = "2.12.11"
   val Scala213Version = "2.13.4"
 
-  val jnaVersion = "4.5.0"
+  val jnaVersion = "5.6.0"
   val nailgunVersion = "ee3c4343"
   val difflibVersion = "1.3.0"
   val caseAppVersion = "1.2.0-faster-compile-time"
@@ -18,7 +18,6 @@ object Dependencies {
   val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.26"
   val jnaPlatform = "net.java.dev.jna" % "jna-platform" % jnaVersion
   val nailgun = "ch.epfl.scala" % "nailgun-server" % nailgunVersion
-  val nailgunExamples = "ch.epfl.scala" % "nailgun-examples" % nailgunVersion
   val difflib = "com.googlecode.java-diff-utils" % "diffutils" % difflibVersion
 
   val scopt = "com.github.scopt" %% "scopt" % "4.0.0-RC2"
